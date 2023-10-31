92d82826f6cddecf8d7ae99741c927832fd3f80b
jshu3796 <jshu3796@uw.edu>
2023-10-31 16:03:01 -0700

    modified:   cli.py 	modified:   logic.py

--- cli.py
+++ cli.py
@@ -2,8 +2,8 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board
-
+from logic import make_empty_board, get_winner
+a="O"
 
 # Reminder to check all the tests
 
@@ -13,7 +13,18 @@ if __name__ == '__main__':
     while winner == None:
         print("TODO: take a turn!")
         # TODO: Show the board to the user.
+        for line in board:
+            print(line)
         # TODO: Input a move from the player.
+        movement=input(f"plz input 'X,Y',no more than 2 and no less the 0. It's {a}'s turn")
+        X,Y=movement.split(',')
+        X=int(X)
+        Y=int(Y)
         # TODO: Update the board.
+        board[X][Y]=a
         # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
+        if a=="O":
+            a="X"
+        elif a=="X":
+            a="O"
+        winner = get_winner(board)  # FIXME
--- logic.py
+++ logic.py
@@ -14,7 +14,27 @@ def make_empty_board():
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    return None  # FIXME
+    for row in board:
+        if row[0]==row[1]==row[2]=='X':
+            return 'X'
+        elif row[0]==row[1]==row[2]=='O':
+            return 'O'
+
+    for a in range(len(board)):
+        if board[0][a]==board[1][a]==board[2][a] == 'X':
+            return 'X'
+        elif board[0][a]==board[1][a]==board[2][a] == 'O':
+            return 'O'
+
+    if all(board[i][i] == 'X' for i in range(3)) or all(board[i][2 - i] == 'X' for i in range(3)):
+        return 'X'
+    elif all(board[i][i] == 'O' for i in range(3)) or all(board[i][2 - i] == 'O' for i in range(3)):
+        return 'O'
+
+    if all(cell != '' for row in board for cell in row):
+        return  None
+
+    return None
 
 
 def other_player(player):
