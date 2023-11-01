418a8c922b1dce17522bb4e4e211fe1abb5e9dc7
jshu3796 <jshu3796@uw.edu>
2023-11-01 16:34:37 -0700

    update

--- cli.py
+++ cli.py
@@ -1,35 +1,52 @@
 # This file contains the Command Line Interface (CLI) for
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
+
 import logging
 from logic import make_empty_board, get_winner
 
 logging.basicConfig(
-    filename = 'logs/infos.log',
-    level = logging.INFO
+    filename='logs/infos.log',
+    level=logging.INFO
 )
 
-# Reminder to check all the tests
+def print_board(board):
+    print("Current Board:")
+    print("-" * 5)
+    for row in board:
+        print(" | ".join(cell if cell is not None else " " for cell in row))
+        print("-" * 5)
 
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
-    a="O"
-    while winner == None:
-        print("TODO: take a turn!")
-        # TODO: Show the board to the user.
-        for line in board:
-            print(line)
-        # TODO: Input a move from the player.
-        movement=input(f"plz input 'X,Y',no more than 2 and no less the 0. It's {a}'s turn")
-        X,Y=movement.split(',')
-        X=int(X)
-        Y=int(Y)
-        # TODO: Update the board.
-        board[X][Y]=a
-        # TODO: Update who's turn it is.
-        if a=="O":
-            a="X"
-        elif a=="X":
-            a="O"
-        winner = get_winner(board)  # FIXM
+    current_player = "O"
+
+    while winner is None:
+        print_board(board)
+
+        movement = input(f"Please input 'row,col' (0 <= row, col <= 2). It's {current_player}'s turn: ")
+        try:
+            row, col = map(int, movement.split(','))
+
+            if board[row][col] is None:
+                board[row][col] = current_player
+            else:
+                print("Invalid move. Cell already occupied. Try again.")
+                continue
+
+            logging.info(f"Board state: {board}")
+            logging.info(f"{current_player} made a move at ({row}, {col})")
+
+            current_player = "X" if current_player == "O" else "O"
+            winner = get_winner(board)
+
+        except ValueError:
+            print("Invalid input format. Please use 'row,col'. Try again.")
+
+    print_board(board)
+
+    if winner:
+        print(f"Player {winner} wins!")
+    else:
+        print("It's a draw!")
--- logic.py
+++ logic.py
@@ -1,8 +1,6 @@
 # This file is where game logic lives. No input
 # or output happens here. The logic in this file
 # should be unit-testable.
-
-
 def make_empty_board():
     return [
         [None, None, None],
@@ -12,31 +10,23 @@ def make_empty_board():
 
 
 def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
     for row in board:
-        if row[0]==row[1]==row[2]=='X':
-            return 'X'
-        elif row[0]==row[1]==row[2]=='O':
-            return 'O'
+        if row[0] == row[1] == row[2] and row[0] is not None:
+            return row[0]
 
     for a in range(len(board)):
-        if board[0][a]==board[1][a]==board[2][a] == 'X':
-            return 'X'
-        elif board[0][a]==board[1][a]==board[2][a] == 'O':
-            return 'O'
+        if board[0][a] == board[1][a] == board[2][a] and board[0][a] is not None:
+            return board[0][a]
 
-    if all(board[i][i] == 'X' for i in range(3)) or all(board[i][2 - i] == 'X' for i in range(3)):
-        return 'X'
-    elif all(board[i][i] == 'O' for i in range(3)) or all(board[i][2 - i] == 'O' for i in range(3)):
-        return 'O'
+    if all(board[i][i] == board[0][0] and board[i][i] is not None for i in range(3)) or all(
+            board[i][2 - i] == board[0][2] and board[i][2 - i] is not None for i in range(3)):
+        return board[1][1]
 
-    if all(cell != '' for row in board for cell in row):
-        return  None
+    if all(cell is not None for row in board for cell in row):
+        return None
 
     return None
 
 
 def other_player(player):
-    """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    return "O" if player == "X" else "X"
